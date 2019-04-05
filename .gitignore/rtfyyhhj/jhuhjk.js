7458861bdf15830c809fc183bd00893de2d305c2
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2019-04-05 16:05:09 +0200

    Update jhuhjk.js

--- .gitignore/rtfyyhhj/jhuhjk.js
+++ .gitignore/rtfyyhhj/jhuhjk.js
@@ -15,14 +15,13 @@ class Rule34Command extends Commando.Command {
                 usages: 999,
                 duration: 1
             },
-            description: 'Posts lewd image from rule34.xxx :sweat_drops:',
+            description: 'Envoie une image cochonne :S',
             examples: [kjhhlolm.prefix + 'r34 loli+small_breasts']
         });
     }
 
     async run(message, args) {
         try {
-            // Currently there is something wrong with Commando nsfw detection... So better make sure this works
             if(message.message.channel.nsfw){
                 if(args[0] === undefined){
                     var argR = "";
@@ -49,23 +48,22 @@ class Rule34Command extends Commando.Command {
                             if(postCount > 0) {
                                 var picNum = Math.floor(Math.random() * postCount) + 0;
                                 var r34Pic = result.posts.post[picNum].$.file_url;
-                                // console.log(result.posts.post[picNum].$.file_url);
                                 message.channel.send({
                                     files: [r34Pic]
                                 });
                             
                             } else {
                                 console.log("Nothing found:", argR);
-                                message.channel.send("Nobody here but us chickens!");
+                                message.channel.send("Je n'ai rien trouvé sur ta demande.");
                             }
 
                             });
                         });
                     }).on('error', function(e){
-                        console.log("Got an error: ", e);
+                        console.log("Erreur, BIP - BIP - Veuillez contacter un administateur.", e);
                 });
             } else {
-                message.channel.send(":warning: This channel is not NSFW!");
+                message.channel.send("Ce channel n'est pas en NSFW !");
             }
         } catch(e) {
             console.log(e);
